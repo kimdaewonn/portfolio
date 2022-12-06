a7fad4092ab154b2c8c480b097f2d9dce37bce4a
kimdaewonn <eodnjs9605@gmail.com>
2022-12-06 16:19:40 +0900

    4:19

--- assets/script/gsap.js
+++ assets/script/gsap.js
@@ -1,13 +1,39 @@
+gsap.registerPlugin(ScrollTrigger);
 // section1
 
 // 오른쪽 다리
-const tl1 = gsap.timeline({repeat: 100, yoyo: true});
-tl1.to(".sec1 .img .img1", {x: 500,y:500, duration: 1, rotation: 30})
-.to(".sec1 .img .img1", {x: 200,y:100, duration: 1, rotation: -30})
-.to(".sec1 .img .img1", {x: 100,y:100, duration: 1, rotation: 20})
+const leg1 = gsap.timeline({repeat: 100, yoyo: true});
+leg1.to(".sec1 .img .img1", {x: 500,y:500, duration: 1.5, rotation: 30})
+.to(".sec1 .img .img1", {x: 200,y:100, duration: 1.5, rotation: -30})
+.to(".sec1 .img .img1", {x: 100,y:100, duration: 1.5, rotation: 20})
 // 왼쪽 다리
-const tl2 = gsap.timeline({repeat: 100, yoyo: true});
-tl2.to(".sec1 .img .img2", {x:-300,y:-500, duration: 1, rotation: 30})
-.to(".sec1 .img .img2", {x: -200,y:100, duration: 1, rotation: -30})
-.to(".sec1 .img .img2", {x: -100,y:100, duration: 1, rotation: 20})
+const leg2 = gsap.timeline({repeat: 100, yoyo: true});
+leg2.to(".sec1 .img .img2", {x:-300,y:-500, duration: 1.5, rotation: 30})
+.to(".sec1 .img .img2", {x: -200,y:100, duration: 1.5, rotation: -30})
+.to(".sec1 .img .img2", {x: -100,y:100, duration: 1.5, rotation: 20})
 
+// 메인 텍스트
+const main_text = gsap.timeline({ yoyo: true});
+main_text.to(".main_text", {x:0,y:1000, duration: 2,opacity:0,})
+.to(".main_text", {x:0,y:1000, duration: 2,opacity:1,})
+.to(".main_text", {x: 0,y:0, duration: 2,})
+
+
+
+
+// gsap.to(".main_text", {
+//     x: 1000,
+//     duration: 3,
+//     scrollTrigger: {
+//         trigger: ".main_text",
+//         start: "top 70%",
+//         end: "top 30%",
+//         markers: {
+//             startColor: "blue",
+//             endColor: "purple",
+//             fontSize: "30px"
+//         },
+//         toggleClass: "red",
+//          toggleActions: "restart pause rssume restet",
+//     },
+// });
