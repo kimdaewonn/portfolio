a7c80a9ab6267b196abda3b1c574f09d2c8e88c8
kimdaewonn <eodnjs9605@gmail.com>
2022-12-06 10:56:36 +0900

    1206

---
+++ assets/script/js.js
@@ -0,0 +1,10 @@
+// 데이터 애니에미션
+const s = skrollr.init();
+
+// 스크롤탑
+function scroll() {
+    let scrollTop = window.screenY || document.documentElement.scrollTop;
+    document.querySelector(".scrollTop").innerText = Math.ceil(scrollTop);
+    requestAnimationFrame(scroll);
+  }
+  scroll();
