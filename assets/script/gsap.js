4ee18cb567efb4c12af5fff0265119c768915653
kimdaewonn <eodnjs9605@gmail.com>
2022-12-08 01:57:48 +0900

    1208

--- assets/script/gsap.js
+++ assets/script/gsap.js
@@ -1,11 +1,31 @@
 gsap.registerPlugin(ScrollTrigger);
 
+gsap.to(".load", {
+    xPercent: 70,
+    duration: 1,
+    scrollTrigger: {
+        trigger: ".load",
+        start: "top 30%",
+        end: "top 80%",
+        scrub: 5,    // 스무스하게 슬라이드 효과 / 숫자로 쓸 수도 있음.
+        markers: {
+            startColor: "blue",
+            endColor: "red",
+            fontSize: "20px"
+        },
+        toggleActions: "restart none none none",
+        scrub : 5,
+    },
+});
+
+
+
 gsap.to(".about1_img1", {
     yPercent: 3,
     duration: 3,
     scrollTrigger: {
         trigger: ".about1_img1",
-        start: "top top",
+        start: "top 30%",
         end: "top 80%",
         scrub: 5,    // 스무스하게 슬라이드 효과 / 숫자로 쓸 수도 있음.
         markers: {
@@ -62,6 +82,39 @@ gsap.to(".about2_img1", {
 
 
 
+
+
+
+
+
+
+
+
+
+        //두개쓰기
+// gsap.to('.load', {
+//     xPercent: -10,opacity:0,duration:0.1,scrub:5,
+//     immediateRender: false,
+//     scrollTrigger: {
+//       trigger: '.load',
+//       scrub: true,
+//       markers: true,
+//     }
+//   });
+
+//   gsap.to('.load', {
+//     xPercent: 10, opacity:1,duration:1,scrub:5,
+//     immediateRender: false,
+//     scrollTrigger: {
+//       trigger: '.load',
+//       scrub: true,
+//       markers: true,
+//     }
+//   });
+
+
+
+    //fromto
 // ScrollTrigger.create({
 //     trigger: ".load",
 //     scrub: 5,
---
+++ assets/script/js.js
@@ -0,0 +1,7 @@
+// document.querySelectorAll(".split").forEach(desc => {
+//     let splitText = desc.innerText;
+//     let splitWrap = splitText.split('').join("</span><span aria-hidden='true'>");
+//         splitWrap = "<span aria-hidden='true'>" + splitWrap + "</span>";
+//         desc.innerHTML = splitWrap;
+//         desc.setAttribute("aria-label", splitText);
+// })
