0005366e15f2bd7fe6a1ca4fdc21b51dcc42e9e0
kimdaewonn <eodnjs9605@gmail.com>
2022-12-08 14:20:34 +0900

    1208

--- assets/script/js.js
+++ assets/script/js.js
@@ -5,8 +5,9 @@ function scroll(){
     document.querySelector("#parallax__info span").innerText = Math.ceil(scrollTop);
 
     document.querySelectorAll(".work").forEach(item =>{
-       const target1 = item.querySelector(".site_right1 .img1");
-       const target2 = item.querySelector(".site_left2 .img1");
+       const target1 = item.querySelectorAll
+       (".site_right1 .img1, .site_left2 .img1");
+      //  const target2 = item.querySelector(".site_left2 .img1");
       //  const target3 = item.querySelector(".content__item__desc");
       //  const target4 = item.querySelector(".content__item__num");
 
@@ -15,7 +16,7 @@ function scroll(){
       //  let offset3 = (scrollTop - item.offsetTop) * 0.2;
 
         gsap.to(target1, {duration: .3, y: offset1, ease: "power4.out"})
-        gsap.to(target2, {duration: .3, y: offset1, ease: "power4.out"})
+        // gsap.to(target2, {duration: .3, y: offset1, ease: "power4.out"})
         // gsap.to(target3, {duration: .3, y: offset2})
         // gsap.to(target4, {duration: .3, y: offset3, ease: "expo.out"})
     });
